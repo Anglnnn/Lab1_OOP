e9b72462831073afbe6f12ca74d3587a1272afa5
Angelina <99400952+Anglnnn@users.noreply.github.com>
2024-05-31 16:44:38 +0300

    Update Lab1ServletApplicationTests.java

--- src/test/java/com/example/lab1_servlet/Lab1ServletApplicationTests.java
+++ src/test/java/com/example/lab1_servlet/Lab1ServletApplicationTests.java
@@ -1,1 +1,14 @@
 
+package com.example.lab1_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
